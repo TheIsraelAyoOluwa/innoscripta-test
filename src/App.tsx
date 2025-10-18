d6bb99df8cf1713d13ff39c1cb7b94eb08ddcf55
TheIsraelAyoOluwa <izzyfresher@gmail.com>
2025-10-18 23:09:03 +0100

    feat: added docker file and config

--- src/App.tsx
+++ src/App.tsx
@@ -96,18 +96,6 @@ function App() {
   const isLoading =
     fetching1 || fetching2 || fetching3 || fetchingSource1 || fetchingSource2;
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="p-8">
-  //       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 place-items-center">
-  //         {Array.from({ length: 8 }).map((_, i) => (
-  //           <NewsCardSkeleton key={i} />
-  //         ))}
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
 
   return (
     <div className="">
